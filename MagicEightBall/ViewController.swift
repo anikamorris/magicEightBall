ac15d15b6692a4f89606f9d5079771794f5391ee
anikamorris <anikamorris11@gmail.com>
2019-11-19 15:58:24 -0800

    app icons added

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Anika Morris on 11/19/19.
+//  Copyright © 2019 Anika Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
