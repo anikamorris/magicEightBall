7b848fe970094361a150278eef00feaefeb3d106
anikamorris <anikamorris11@gmail.com>
2019-11-19 16:32:02 -0800

    did stretch challenge

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let companies = ["Make School", "Uber", "Netflix", "Facebook", "Google", "Kickstarter", "Spotify", "Airbnb", "Snapchat", "YouTube", "Dropbox", "Amazon", "Craigslist", "Tinder", "Instagram", "Tesla", "Twitter", "SpaceX"]
 
-    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    let customers = ["Dogs", "Books", "Gamers", "Star Wars", "Bitcoin", "Goats", "Zombies", "Rich People", "Swimmers", "Florida", "Vampires", "Dragons", "Internet of Things", "Mars", "Cryptocurrency", "Mosquito Repellent", "Fidget Spinners", "Sun Screen", "Water Bottles", "Lost Travelers", "Superheroes"]
+
 
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var shakeButton: UIButton!
+    @IBOutlet weak var customerLabel: UILabel!
+    @IBOutlet weak var forLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        customerLabel.text = ""
+        forLabel.text = ""
     }
     
     override func didReceiveMemoryWarning() {
@@ -35,9 +42,12 @@ class ViewController: UIViewController {
     }
     
     func generateAnswer() {
-        let randomIndex = Int.random(in: 0..<answers.count)
+        let randomCompany = Int.random(in: 0..<companies.count)
+        let randomCustomer = Int.random(in: 0..<customers.count)
         
-        answerLabel.text = answers[randomIndex]
+        answerLabel.text = companies[randomCompany]
+        forLabel.text = "for"
+        customerLabel.text = customers[randomCustomer]
     }
 }
 
